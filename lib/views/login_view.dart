e5bdaf3aa0a32c6678cd9050d911801eb686693a
rfks <rfks@freemail.hu>
2022-09-22 23:03:48 +0200

    step-6

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -4,6 +4,8 @@ import 'dart:developer' as devtools show log;
 
 import 'package:ttm/constants/routes.dart';
 
+import '../utilities/show_error_dialog.dart';
+
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
 
@@ -70,10 +72,26 @@ class _LoginViewState extends State<LoginView> {
                 );
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'user-not-found') {
-                  devtools.log('User not found!');
+                  await showErrorDialog(
+                    context,
+                    'User not found!',
+                  );
                 } else if (e.code == 'wrong-password') {
-                  devtools.log('Wrong password!');
+                  await showErrorDialog(
+                    context,
+                    'Wrong password!',
+                  );
+                } else {
+                  await showErrorDialog(
+                    context,
+                    'Error: ${e.code}',
+                  );
                 }
+              } catch (e) {
+                await showErrorDialog(
+                  context,
+                  e.toString(),
+                );
               }
             },
             child: const Text('Login'),
