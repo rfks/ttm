48e6c4ba7aaadc422906e0827f713a036e6f4f15
rfks <rfks@freemail.hu>
2022-10-24 00:04:08 +0200

    unsaved files..

--- lib/utilities/dialogs/logout_dialog.dart
+++ lib/utilities/dialogs/logout_dialog.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ttm/utilities/dialogs/generic_dialog.dart';
 
 Future<bool> showLogoutDialog(BuildContext context) {
-  return showGenericDialog(
+  return showGenericDialog<bool>(
     context: context,
     title: 'Logout',
     content: 'Are you sure you want to logout?',
