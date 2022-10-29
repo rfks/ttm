35b84b3b8f7decb17b74837dab7eadf3065b9606
rfks <rfks@freemail.hu>
2022-10-29 11:53:39 +0200

    step-17

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ttm/constants/routes.dart';
 import 'package:ttm/services/auth/auth_service.dart';
 import 'package:ttm/views/login_view.dart';
-import 'package:ttm/views/notes/new_note_view.dart';
+import 'package:ttm/views/notes/create_update_note_view.dart';
 import 'package:ttm/views/notes/notes_view.dart';
 import 'package:ttm/views/register_view.dart';
 import 'package:ttm/views/verify_email_view.dart';
@@ -20,7 +20,7 @@ void main() {
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
         verifyEmailRoute: (context) => const VerifyEmailView(),
-        newNoteRoute: (context) => const NewNoteView(),
+        createOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
       },
     ),
   );
