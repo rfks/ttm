6916a41ac1577af5d38259a05bf916a041277a1a
rfks <rfks@freemail.hu>
2022-11-04 00:19:29 +0100

    step-21

--- lib/views/notes/create_update_note_view.dart
+++ lib/views/notes/create_update_note_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:ttm/services/auth/auth_service.dart';
+import 'package:ttm/utilities/dialogs/cannot_share_empty_note_dialog.dart';
 import 'package:ttm/utilities/generics/get_arguments.dart';
 import 'package:ttm/services/cloud/cloud_note.dart';
 import 'package:ttm/services/cloud/cloud_storage_exceptions.dart';
@@ -91,6 +93,19 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('New Note'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              final text = _textController.text;
+              if (_note == null || text.isEmpty) {
+                await showCannotShareEmptyNoteDialog(context);
+              } else {
+                Share.share(text);
+              }
+            },
+            icon: const Icon(Icons.share),
+          )
+        ],
       ),
       body: FutureBuilder(
         future: createOrGetExistingNote(context),
